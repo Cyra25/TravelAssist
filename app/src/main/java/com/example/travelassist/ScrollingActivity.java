c09f13a323efe6eedd6fea0c8925b603ecac4f83
Cyra25 <44994077+Cyra25@users.noreply.github.com>
2019-04-18 17:21:22 +0800

    delete unnecessary comments

--- app/src/main/java/com/example/travelassist/ScrollingActivity.java
+++ app/src/main/java/com/example/travelassist/ScrollingActivity.java
@@ -435,12 +435,9 @@ public class ScrollingActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, setting.class);
             startActivity(intent);
