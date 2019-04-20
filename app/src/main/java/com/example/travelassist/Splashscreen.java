82fb93c65e4e967d3bc4afe435fa364d7c786d17
Cyra25 <44994077+Cyra25@users.noreply.github.com>
2019-04-20 19:05:38 +0800

    Final version with no error :3 Yayyy

---
+++ app/src/main/java/com/example/travelassist/Splashscreen.java
@@ -0,0 +1,53 @@
+package com.example.travelassist;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.widget.ImageView;
+
+
+import java.util.Random;
+
+
+public class Splashscreen extends Activity {
+
+    Thread splashTread;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        imageView = (ImageView)findViewById(R.id.imageView2);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        int[] ids = new int[]{R.drawable.splash1,R.drawable.splash2, R.drawable.splash3};
+        Random randomGenerator = new Random();
+        int r= randomGenerator.nextInt(ids.length);
+        this.imageView.setImageDrawable(getResources().getDrawable(ids[r]));
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 2500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(Splashscreen.this,
+                            ScrollingActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    Splashscreen.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    Splashscreen.this.finish();
+                }
+
+            }
+        };
+        splashTread.start();
+    }
+
+}
